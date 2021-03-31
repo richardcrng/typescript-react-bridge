425ad93716f9a29b175a78b3dac277bb024342eb
Richard Ng <richardcrng@gmail.com>
2021-03-31 08:15:13 +0100

    Add typing variables unions exercise

---
+++ exercises/05-typing-variables/06-typing-variables-unions.ts
@@ -0,0 +1,43 @@
+/* Lesson: Typing variables: union types (1/1)
+ *
+ * Learning outcomes:
+ *  - Add explicit types to a variable declaration
+ *  - Explain how explicit typing restricts what can be assigned to a variable
+ *  - Articulate how explicit typing is different to inferred typing
+ *  - Explain a trade-off between explicit typing and inferred typing
+ *
+ * Instructions:
+ *  1. 👁️ Read the code
+ *  2. 🤔 Predict what will happen when you run the code
+ *  3. 👟 Run the code and compare your prediction
+ *  4. 👩🏽‍🔬 Experiment with changes (change -> predict -> run)
+ *  5. 🧠 Try to complete the challenge(s)
+ *
+ * Study questions:
+ *  1. How might you interpret the pipe (|) operator as an
+ *      ordinary English word that makes sense of the typing
+ *      boolean | string and its behaviour?
+ *
+ *  2. What do you see when you hover over a variable like
+ *
+ */
+
+let boolOrString: boolean | string;
+
+/* 🧠 Which of these gives a TypeScript error, and why? */
+boolOrString = false;
+boolOrString = "false";
+boolOrString = 0;
+
+/* 🧠 Make the type a union type to remove the error */
+let testOne: boolean = 44;
+
+/* 🧠 Add a union type to remove the TypeScript error */
+let testTwo = 3;
+testTwo = "3";
+
+/* 🧠 Make the type a union of three to remove the TypeScript error */
+let testThree: string | boolean;
+testThree = "hi";
+testThree = true;
+testThree = -10;
